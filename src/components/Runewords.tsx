8cbc737cf979f6430159eaf492d429325b29958b
Geoffroy Warin <code@geowarin.com>
2021-10-23 05:10:56 +0200

    table

--- src/components/Runewords.tsx
+++ src/components/Runewords.tsx
@@ -1,49 +1,91 @@
 import runewords from "../data/runewords.json";
-import React from "react";
+import React, { useMemo } from "react";
 import { RunePng, selectedRuneState } from "./RunePng";
 import { useRecoilState } from "recoil";
+import { Column, useTable } from "react-table";
 
 type RunewordType = typeof runewords[number];
 
-function Runeword({ runeword }: { runeword: RunewordType }): JSX.Element {
-  return (
-    <div className="flex pt-3">
-      <div className="w-30">
-        {runeword.type.split("/").map((type) => (
-          <div>
-            <img width={20} src={`/img/icons/${type}.svg`} />
-            {type}
-          </div>
-        ))}
-      </div>
-      <div className="flex flex-col w-1/3">
-        <div>{runeword.name}</div>
-        <div className="flex">
-          {runeword.runes.map((rune) => (
-            <RunePng name={rune} />
-          ))}
-        </div>
-      </div>
-      <div>
-        {runeword.stats.map((stat) => (
-          <div>{stat}</div>
-        ))}
-      </div>
-    </div>
-  );
-}
-
 export function Runewords(): JSX.Element {
   const [selectedRune] = useRecoilState(selectedRuneState);
-  const selected = runewords.filter((runeworld) =>
+  const data = runewords.filter((runeworld) =>
     runeworld.runes.includes(selectedRune ?? "")
   );
+  const columns: Column<RunewordType>[] = useMemo(
+    () => [
+      {
+        Header: "Name",
+        accessor: (runeword) => runeword.name,
+      },
+      {
+        Header: "Type",
+        accessor: (runeword) =>
+          runeword.type.split("/").map((type) => (
+            <div>
+              <img width={20} src={`/img/icons/${type}.svg`} />
+              {type}
+            </div>
+          )),
+      },
+      {
+        Header: "Runes",
+        accessor: (runeword) => (
+          <div className="flex">
+            {runeword.runes.map((rune) => (
+              <RunePng name={rune} />
+            ))}
+          </div>
+        ),
+      },
+      {
+        Header: "Stats",
+        accessor: (runeword) => runeword.stats.map((stat) => <div>{stat}</div>),
+      },
+    ],
+    []
+  );
+
+  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
+    useTable({ columns, data });
 
   return (
-    <div>
-      {selected.map((runeword) => {
-        return <Runeword runeword={runeword} />;
-      })}
-    </div>
+    <table {...getTableProps()} style={{ border: "solid 1px gray" }}>
+      <thead>
+        {headerGroups.map((headerGroup) => (
+          <tr {...headerGroup.getHeaderGroupProps()}>
+            {headerGroup.headers.map((column) => (
+              <th {...column.getHeaderProps()} className="header">
+                {column.render("Header")}
+              </th>
+            ))}
+          </tr>
+        ))}
+      </thead>
+
+      <tbody {...getTableBodyProps()}>
+        {rows.map((row) => {
+          prepareRow(row);
+
+          return (
+            <tr {...row.getRowProps()}>
+              {row.cells.map((cell) => {
+                return (
+                  <td
+                    {...cell.getCellProps()}
+                    style={{
+                      padding: "10px",
+                      border: "solid 1px gray",
+                      // background: "papayawhip",
+                    }}
+                  >
+                    {cell.render("Cell")}
+                  </td>
+                );
+              })}
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
   );
 }
