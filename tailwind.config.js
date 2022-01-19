4c47339e3aa0ba2b267b67c7f341ec99bc80629c
Geoffroy Warin <code@geowarin.com>
2022-01-19 21:41:47 +0100

    update to tailwind 3.0

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,6 @@
 module.exports = {
-  purge: ["./index.html", "./src/**/*.{ts,tsx}"],
-  darkMode: false, // or 'media' or 'class'
+  content: ["src/**/*.tsx"],
   theme: {
-    extend: {},
-  },
-  variants: {
     extend: {},
   },
   plugins: [],
